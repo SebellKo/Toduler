723193d8348145fb0b5bcac5fad5ff97adbfc4f1
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-02-17 04:36:46 +0900

    [feat]: 날짜 전역 스토어 생성

---
+++ src/stores/dateStore.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand';
+import { formatDate } from '../utils/formatDate';
+import dayjs from 'dayjs';
+
+interface Store {
+  selectedDate: string;
+  setSelectedDate: (value: string) => void;
+}
+
+export const useDateStore = create<Store>((set) => ({
+  selectedDate: formatDate(dayjs()),
+  setSelectedDate: (newDate) => ({ selectedDate: newDate }),
+}));
