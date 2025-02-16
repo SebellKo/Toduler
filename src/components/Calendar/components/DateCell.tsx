01e844b088e2fba91ed030cb21cf2af5e1cf9a7a
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-02-16 10:58:51 +0900

    [style]: 달력 컴포넌트 UI 구현

---
+++ src/components/Calendar/components/DateCell.tsx
@@ -0,0 +1,35 @@
+import { Badge } from 'antd';
+import styles from '../../../styles/schedule-calendar.module.css';
+
+interface Props {
+  id: number;
+  date: string;
+  done: number;
+  total: number;
+  schedule: {
+    id: number;
+    time: string;
+    content: string;
+  }[];
+}
+
+function DateCell(item: Props) {
+  const { schedule, done, total } = item;
+  return (
+    <ul className={styles.events}>
+      <li>
+        <Badge
+          status={done === total ? 'success' : 'processing'}
+          text={`${done} of ${total} Completed`}
+        ></Badge>
+      </li>
+      {schedule.map((item) => (
+        <li key={item.id}>
+          <Badge status="error" text={item.content}></Badge>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default DateCell;
