43a55fb212ee4dce5eeee4f070620ef0d5e3f353
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-02-16 11:03:44 +0900

    [refactor]: ToDoListHeader title props 추가

--- src/components/Todo/ToDoListHeader.tsx
+++ src/components/Todo/ToDoListHeader.tsx
@@ -1,11 +1,15 @@
 import { PlusCircleFilled } from '@ant-design/icons';
 import { Flex, Typography, Button } from 'antd';
 
-function ToDoListHeader() {
+interface Props {
+  title: string;
+}
+
+function ToDoListHeader({ title }: Props) {
   return (
     <Flex flex={1} align="center" justify="space-between">
       <Typography.Title level={4} style={{ margin: 0 }}>
-        Todo
+        {title}
       </Typography.Title>
       <Button shape="circle" icon={<PlusCircleFilled />}></Button>
     </Flex>
