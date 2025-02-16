0bd66b356659d15f639e342e7a21f617b36eaebe
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-02-16 15:31:50 +0900

    [feat]: 투두리스트 이름 UI 변경 구현

--- src/components/Todo/ToDoListHeader.tsx
+++ src/components/Todo/ToDoListHeader.tsx
@@ -1,15 +1,22 @@
 import { PlusCircleFilled } from '@ant-design/icons';
 import { Flex, Typography, Button } from 'antd';
+import { useState } from 'react';
 
 interface Props {
   title: string;
 }
 
 function ToDoListHeader({ title }: Props) {
+  const [text, setText] = useState<string>(title);
+  const [editedText, setEditedText] = useState<string>('');
   return (
     <Flex flex={1} align="center" justify="space-between">
-      <Typography.Title level={4} style={{ margin: 0 }}>
-        {title}
+      <Typography.Title
+        level={4}
+        editable={{ onChange: setEditedText, onEnd: () => setText(editedText) }}
+        style={{ margin: 0 }}
+      >
+        {text}
       </Typography.Title>
       <Button shape="circle" icon={<PlusCircleFilled />}></Button>
     </Flex>
