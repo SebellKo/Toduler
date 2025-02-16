021657b56f7feb0a8978c7e67cfd00a725ca92c3
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-02-17 06:13:50 +0900

    [chore]: 미사용 변수 제거

--- src/components/Calendar/ScheduleCalendar.tsx
+++ src/components/Calendar/ScheduleCalendar.tsx
@@ -4,44 +4,7 @@ import DateCell from './DateCell';
 import { useDateStore } from '../../stores/dateStore';
 import { formatDate } from '../../utils/formatDate';
 import { useQuery } from '@tanstack/react-query';
-import { getTodoData } from '../api/getTodoData';
 import { getAllToDos } from '../api/getAllToDos';
-
-type ListData = {
-  id: number;
-  date: string;
-  done: number;
-  total: number;
-  schedule: {
-    id: number;
-    time: string;
-    content: string;
-  }[];
-};
-
-const listData: ListData[] = [
-  {
-    id: 0,
-    date: '2025.02.16.Sun',
-    done: 3,
-    total: 3,
-    schedule: [{ id: 0, time: '18:00', content: 'meeting' }],
-  },
-  {
-    id: 1,
-    date: '2025.02.17.Mon',
-    done: 1,
-    total: 10,
-    schedule: [{ id: 0, time: '18:00', content: 'meeting' }],
-  },
-  {
-    id: 2,
-    date: '2025.02.18.Wen',
-    done: 2,
-    total: 10,
-    schedule: [{ id: 0, time: '18:00', content: 'meeting' }],
-  },
-];
 
 function ScheduleCalendar() {
   const setSelectedDate = useDateStore((state) => state.setSelectedDate);
